2cfb6b5081b2090a8515ed2252792a6adfb14d10
Michelle Olivieri <michelleo72@gmail.com>
2018-03-22 01:37:04 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,10 @@ def key_for_min_value(name_hash)
 
   lowest_value = 0
 
-  name_hash.each do |key, value|
+  name_hash.each do |name, value|
     if value < lowest_value
       lowest_value = value
-      lowest_key = key
+      lowest_key = name
     end
 
   end
